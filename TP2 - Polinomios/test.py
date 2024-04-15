95164450535dd07540513e1a5ebe4a8976e3609a
NicoBaranow <nicolasbaranow@gmail.com>
2024-04-15 00:37:41 -0300

    root finding polis

--- TP2 - Polinomios/test.py
+++ TP2 - Polinomios/test.py
@@ -66,6 +66,29 @@ class poly:
     def __rsub__(self, other):
         return self.__sub__(other)
 
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            # Crear un polinomio de grado 0 con el valor del escalar
+            scalar_poly = poly(0, [other])
+            # Multiplicar el polinomio de grado 0 por el polinomio actual
+            return self * scalar_poly
+        
+        elif isinstance(other, poly):
+            # Multiplicación de polinomio por otro polinomio
+            result_coefs = [0] * (self.n + other.n + 1)
+            for i in range(self.n + 1):
+                for j in range(other.n + 1):
+                    result_coefs[i + j] += self.coefs[i] * other.coefs[j]
+            return poly(self.n + other.n, result_coefs)
+        
+        else:
+            raise TypeError("Unsupported operand type for *: {}".format(type(other)))
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    #Missing __floordiv__; __rfloordiv__; __mod__; __rmod__.
+
     def get_expression(self):
         expression = "p(x) = "
 
@@ -79,6 +102,7 @@ class poly:
         return expression
     
     def poly_plot(self, a, b, **kwargs):
+        
         x_values = np.linspace(a, b, 100000)
         y_values = self(x_values)
         plt.plot(x_values, y_values, **kwargs)
@@ -87,9 +111,72 @@ class poly:
         plt.title('Grafico del polinomio')
         plt.grid(True)
         plt.show()
+    
+    def derivada (self):
+        derivative_coefs = [self.coefs[i] * i for i in range(1, self.n + 1)]
+        return poly(self.n - 1, derivative_coefs)
 
-a = poly(n, coefs)
-b = poly(5,[2,51,51,4,6,32])
+    def root_find_bisection(self,a,b, tolerance = 1e-5, iter = 1000):
+        '''
+        Toma como parametro dos valores entre los cuales buscar la raiz real del polinomio; tolerancia del 0; iteraciones.
+        a*b <= 0. 
+        Devuelve una raiz real del polinomio. Caso contrario, da error
+        '''
+        iter_count = 0
+        if self(a) * self(b) > 0: raise ValueError("No se puede garantizar la existencia de una raíz en el intervalo dado.")
 
-newPoly =  b-a
-print(newPoly.get_expression())
+        while iter_count < iter:
+            c = (a+b) / 2
+            if abs(c) < tolerance: return c
+            elif self(a) * self(c) < 0: b = c
+            else: a = c
+            iter_count += 1
+        
+        raise ValueError(f"El método de bisección no convergió después de {iter} iteraciones.")
+    
+    def root_find_newton(self, x0, tolerance = 1e-5, iter = 1000):
+        iter_count = 0
+        x = x0
+        while iter_count < iter:
+            f_x = self(x)
+
+            if abs(f_x) < tolerance:
+                return x
+            
+            f_prime_x = self.derivada()(x)
+            if f_prime_x == 0:
+                raise ValueError(f"Derivada en el punto {x} es cero.")
+            x = x - f_x / f_prime_x
+            iter_count += 1
+
+        raise ValueError(f"El método de Newton-Raphson no convergió después de {iter} iteraciones.")
+
+    def findroots(self):
+        roots = []
+        residual_poly = self
+        while True:
+            try:
+                # Buscar una raíz utilizando el método de Newton-Raphson
+                root = residual_poly.root_find_newton(0)
+                # Determinar la multiplicidad de la raíz
+                multiplicity = 1
+                while True:
+                    # Dividir el polinomio residual por el monomio (x - root)
+                    divisor = poly(1, [-root, 1])
+                    residual_poly = residual_poly // divisor
+                    # Si el residuo no es cero, la raíz tiene una multiplicidad mayor
+                    if residual_poly != poly(0):
+                        multiplicity += 1
+                    else:
+                        break
+                # Agregar la raíz y su multiplicidad a la lista de raíces
+                roots.append((root, multiplicity))
+            except ValueError:
+                # Si no se puede encontrar más raíces, terminar el bucle
+                break
+        return roots, residual_poly
+
+a = poly(4, [2, -6, -11, 9, 1])
+b = poly(2, [-2,0,3])
+root = a.findroots()
+print(root)
